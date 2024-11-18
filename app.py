93b4fa9e68d6af3778f9fdb9d1e1011dccde1486
JW5123 <Jay2002714@gmail.com>
2024-11-19 02:25:37 +0800

    Update main branch file

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from student_course.student_course import student_course_bp
 from student_course.student_list import student_list_bp
 from course_list.course_list import course_list_bp
 from course_list.create_course import create_course_bp
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'efb658580b34b7c6a6e15208b8ba545d'
+load_dotenv()
 
 app.register_blueprint(account_mgmt_bp, url_prefix='/account')
 app.register_blueprint(login_bp)
